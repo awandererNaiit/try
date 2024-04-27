8aace3685c9f4e7f40b5b8431fad514043054b17
Maksim Bodnar <maximsevsk@gmail.com>
2024-04-27 17:53:04 +0300

    проверка работы git

--- main.py
+++ main.py
@@ -2,54 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class BaseProduct(ABC):
-    def __init__(self, name, description, price, amount):
-        self._name = name
-        self._description = description
-        self._price = price
-        self._amount = amount
-
-    @abstractmethod
-    def specific_info(self):
-        pass
-
     @abstractmethod
     def new_product(self, *args):
         pass
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}('{self._name}', '{self._description}', {self._price}, {self._amount})"
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def description(self):
-        return self._description
-
-    @property
-    def price(self):
-        return self._price
-
-    @property
-    def amount(self):
-        return self._amount
-
-    @name.setter
-    def name(self, value):
-        self._name = value
-
-    @description.setter
-    def description(self, value):
-        self._description = value
-
-    @price.setter
-    def price(self, value):
-        self._price = value
-
-    @amount.setter
-    def amount(self, value):
-        self._amount = value
 
 
 class ProductCreationMixin:
@@ -62,52 +17,13 @@ class ProductCreationMixin:
         return f"создан объект со свойствами: {object_attributes}"
 
 
-class Category:
-    total_categories = 0
-    total_unique_products = 0
-
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self._products = []
-        Category.total_categories += 1
-
-    @property
-    def products(self):
-        return self._products
-
-    @products.setter
-    def products(self, products):
-        self._products = products
-
-    def add_product(self, product):
-        if not isinstance(product, BaseProduct):
-            raise TypeError("Нельзя добавлять разные продукты")
-        self._products.append(product)
-        Category.total_unique_products += 1
-
-    def remove_product(self, product):
-        self._products.remove(product)
-        Category.total_unique_products -= 1
-
-    def get_products_info(self):
-        products_info = []
-        for product in self._products:
-            product_info = f"{product.name}, {product.price} руб. Остаток: {product.amount} шт."
-            products_info.append(product_info)
-        return products_info
-
-    def __str__(self):
-        total_products = len(self)
-        return f"{self.name}, количество продуктов: {total_products} шт."
-
-    def __len__(self):
-        return sum(product.amount for product in self._products)
-
-
 class Smartphone(BaseProduct, ProductCreationMixin):
     def __init__(self, name, description, price, amount, brand, model, storage, color):
-        super().__init__(name, description, price, amount)
+        super().__init__()
+        self._name = name
+        self._description = description
+        self._price = price
+        self._amount = amount
         self._brand = brand
         self._model = model
         self._storage = storage
@@ -122,7 +38,11 @@ class Smartphone(BaseProduct, ProductCreationMixin):
 
 class LawnGrass(BaseProduct, ProductCreationMixin):
     def __init__(self, name, description, price, amount, origin, duration, color):
-        super().__init__(name, description, price, amount)
+        super().__init__()
+        self._name = name
+        self._description = description
+        self._price = price
+        self._amount = amount
         self._origin = origin
         self._duration = duration
         self._color = color
