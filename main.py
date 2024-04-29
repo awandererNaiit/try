43ce458e2fd77a742dc97e6512e323d13d6478cc
Maksim Bodnar <maximsevsk@gmail.com>
2024-04-29 13:58:05 +0300

    -_-

--- main.py
+++ main.py
@@ -2,22 +2,70 @@ from abc import ABC, abstractmethod
 
 
 class BaseProduct(ABC):
-    def __init__(self, name, description, price, amount):
-        self._name = name
-        self._description = description
-        self._price = price
-        self._amount = amount
-
-    def __repr__(self):
-        object_attributes = ', '.join(f"{k}={v}" for k, v in self.__dict__.items())
-        return f"создан объект со свойствами: {object_attributes}"
-
     @abstractmethod
-    def specific_info(self):
+    def new_product(self, *args):
         pass
 
 
-class Smartphone(BaseProduct):
+class PrintMixin:
+
+    def __init__(self, *args):
+        print(repr(self))
+
+    def __repr__(self):
+        object_attributes = ''
+        for k, v in self.__dict__.items():
+            object_attributes += f'{k}: {v},'
+        return f"создан объект со свойствами {object_attributes})"
+
+
+class Product(BaseProduct, PrintMixin):
+    """
+    Класс для описания товара в магазине
+    """
+
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        self.name = name
+        self.description = description
+        self._price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.\n'
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        return cls(**product_data)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @description.setter
+    def description(self, value):
+        self._description = value
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name='{self.name}', price={self.price}, amount={self.amount})"
+
+
+class Smartphone(Product):
     def __init__(self, name, description, price, amount, brand, model, storage, color):
         super().__init__(name, description, price, amount)
         self._brand = brand
@@ -25,16 +73,10 @@ class Smartphone(BaseProduct):
         self._storage = storage
         self._color = color
 
-    def specific_info(self):
-        return f"Brand: {self._brand}, Model: {self._model}, Storage: {self._storage}, Color: {self._color}"
 
-
-class LawnGrass(BaseProduct):
+class LawnGrass(Product):
     def __init__(self, name, description, price, amount, origin, duration, color):
         super().__init__(name, description, price, amount)
         self._origin = origin
         self._duration = duration
         self._color = color
-
-    def specific_info(self):
-        return f"Origin: {self._origin}, Duration: {self._duration}, Color: {self._color}"
